47b653a17d85d2f6d90e6649ed4881b76ab1b18e
r0982749 <r0982749@student.vives.be>
2024-03-27 15:15:34 +0100

    labo 6 fix

--- sem2/labo6/opdrachtColorpicker/scripts/code.js
+++ sem2/labo6/opdrachtColorpicker/scripts/code.js
@@ -26,8 +26,18 @@ const updateColorField = () => {
 
 const saveColor = () => {
     let color = document.getElementById("colorField").style.backgroundColor;
+    let red = color.substring(color.indexOf("(") + 1, color.indexOf(","));
+    let tempColor = color.substring(color.indexOf(", ") + 2);
+    let green = tempColor.substring(0, tempColor.indexOf(","));
+    tempColor = tempColor.substring(tempColor.indexOf(", ") + 2);
+    let blue = tempColor.substring(0, tempColor.indexOf(")"));
+
+
 
     let saveDiv = document.createElement("div");
+    saveDiv.setAttribute("data-red-value", red);
+    saveDiv.setAttribute("data-green-value", green);
+    saveDiv.setAttribute("data-blue-value", blue);
     saveDiv.classList.add("save-div");
     saveDiv.style.backgroundColor = color;
 
@@ -42,7 +52,17 @@ const saveColor = () => {
     buttonDiv.appendChild(removeButton);
     saveDiv.appendChild(buttonDiv);
 
+    saveDiv.addEventListener("click", restoreColor)
     document.getElementById("save-field").appendChild(saveDiv);
+}
+
+const restoreColor = (event) => {
+    let saveDiv = event.target;
+    document.getElementById("redSlider").value = saveDiv.getAttribute("data-red-value");
+    document.getElementById("greenSlider").value = saveDiv.getAttribute("data-green-value");
+    document.getElementById("blueSlider").value = saveDiv.getAttribute("data-blue-value");
+
+    updateColorField();
 }
 
 const removeColorSave = (event) => {
